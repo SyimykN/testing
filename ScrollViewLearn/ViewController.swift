572100834850ac24cf7aff7e4fba15e682a2c0f8
Syimyk Nurdinov <syimykdev@gmail.com>
2021-08-23 11:07:24 +0600

    merge new feature to master

--- ScrollViewLearn/ViewController.swift
+++ ScrollViewLearn/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UIViewController {
     
     func newMasterFunc() {
         print("Some text!")
+    }
+    
     func newFunctionFromNewFeatureBranch() {
         print("HEllo world!")
     }
