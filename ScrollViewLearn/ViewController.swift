3631079df118f4e13916f63c42215b49a2e51977
Syimyk Nurdinov <syimykdev@gmail.com>
2021-08-23 10:57:57 +0600

    initial commit

---
+++ ScrollViewLearn/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScrollViewLearn
+//
+//  Created by Sema on 24/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var myScrollView = UIScrollView()
+    var myImageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let imageToLoad = UIImage(named: "avengers")
+        myImageView = UIImageView(image: imageToLoad)
+        myScrollView = UIScrollView(frame: self.view.bounds)
+        myScrollView.addSubview(myImageView)
+        myScrollView.contentSize = self.myImageView.bounds.size
+        self.view.addSubview(myScrollView)
+    }
+}
+
